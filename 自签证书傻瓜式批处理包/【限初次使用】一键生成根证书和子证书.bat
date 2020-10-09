0a7a897fa65079858d46278d064750d82b50d0f7
ShioMile <72609717+ShioMile@users.noreply.github.com>
2020-10-09 21:44:55 +0800

    fix bat

--- 自签证书傻瓜式批处理包/【限初次使用】一键生成根证书和子证书.bat
+++ 自签证书傻瓜式批处理包/【限初次使用】一键生成根证书和子证书.bat
@@ -1,3 +1,5 @@
+echo 01 > crtserial.srl
+break > index.txt
 openssl genrsa -out pixiv.net.key 2048
 openssl genrsa -out rootCA.key 2048
 openssl req -new -x509 -key rootCA.key -out rootCA.crt -days 3650 -config config_rootCA.txt
