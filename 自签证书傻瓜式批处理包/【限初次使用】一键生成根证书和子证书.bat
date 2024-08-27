9b3303faa009635da86d77efe40cc972cf2493a3
c15412 <70379135+c15412@users.noreply.github.com>
2024-08-27 18:36:53 +0800

    Update the self-signed certificate config
    self-signed certificate

--- 自签证书傻瓜式批处理包/【限初次使用】一键生成根证书和子证书.bat
+++ 自签证书傻瓜式批处理包/【限初次使用】一键生成根证书和子证书.bat
@@ -1,7 +1,8 @@
 echo 01 > crtserial.srl
 break > index.txt
-openssl genrsa -out pixiv.net.key 2048
-openssl genrsa -out rootCA.key 2048
-openssl req -new -x509 -key rootCA.key -out rootCA.crt -days 3650 -config config_rootCA.txt
-openssl req -new -sha256 -key pixiv.net.key -out pixiv.net.csr -config config_childCA.txt
-openssl ca -config config_signCA.txt -in pixiv.net.csr -out pixiv.net.crt
+.\openssl\openssl genrsa -out pixiv.net.key 2048
+.\openssl\openssl genrsa -out rootCA.key 2048
+.\openssl\openssl req -new -x509 -key rootCA.key -out rootCA.crt -days 3650 -config config_rootCA.txt
+.\openssl\openssl req -new -sha256 -key pixiv.net.key -out pixiv.net.csr -config config_childCA.txt
+.\openssl\openssl ca -config config_signCA.txt -in pixiv.net.csr -out pixiv.net.crt
+pause
