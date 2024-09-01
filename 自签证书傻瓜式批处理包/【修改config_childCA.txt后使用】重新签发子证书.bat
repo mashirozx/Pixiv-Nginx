81c7a11ef8fea87ea35846f67b8eb24a35cbe8b9
c15412 <70379135+c15412@users.noreply.github.com>
2024-09-01 13:09:39 +0800

    更新 (#69)
    * Add files via upload
    
    Update the upstream of Steam, Pixiv, E-hentai
    Add Server :
    Discord (IPv4)
    Instagram (IPv6 Only, Local DNS rewrite required to rewrite all subdomains)
    WallHaven (IPv4, image CDN Only, excluding pages)
    Reddit (IPv4, Local DNS rewrite required to rewrite all subdomains)
    Twitter (IPv4, Local DNS rewrite required to rewrite all subdomains)
    Twitch (IPv4, Local DNS And DoH were required to rewrite all subdomains)
    GoogleCDN (File Server IPv6 Only, website pages and IPv4 servers were not included, Local DNS rewrite required to rewrite all subdomains)
    
    * Update the self-signed certificate config
    
    self-signed certificate
    
    * Update README.md
    
    * 更换NGINX版本
    
    * 配置文件（2024年08月31日18时11分
    
    配置文件（2024年08月31日18时11分
    １、更新部分上游服务器
    ２、改进KeepAlive参数，提升访问可靠性

--- 自签证书傻瓜式批处理包/【修改config_childCA.txt后使用】重新签发子证书.bat
+++ 自签证书傻瓜式批处理包/【修改config_childCA.txt后使用】重新签发子证书.bat
@@ -1,4 +1,5 @@
 del /f /s /q pixiv.net.csr
 del /f /s /q pixiv.net.crt
-openssl req -new -sha256 -key pixiv.net.key -out pixiv.net.csr -config config_childCA.txt
-openssl ca -config config_signCA.txt -in pixiv.net.csr -out pixiv.net.crt
+.\openssl\openssl req -new -sha256 -key pixiv.net.key -out pixiv.net.csr -config config_childCA.txt
+.\openssl\openssl ca -config config_signCA.txt -in pixiv.net.csr -out pixiv.net.crt
+pause
