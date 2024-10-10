a6a6d9ae16dc1fdad4587cd4c6109a67f428d522
Space Time <Zeus6_6@163.com>
2024-10-10 13:26:21 +0800

    feat: 使用 Sheas Nginx 作为启动器

--- 自签证书傻瓜式批处理包/【修改config_childCA.txt后使用】重新签发子证书.bat
+++
@@ -1,5 +0,0 @@
-del /f /s /q pixiv.net.csr
-del /f /s /q pixiv.net.crt
-.\openssl\openssl req -new -sha256 -key pixiv.net.key -out pixiv.net.csr -config config_childCA.txt
-.\openssl\openssl ca -config config_signCA.txt -in pixiv.net.csr -out pixiv.net.crt
-pause
